2b68b3cebc9bd3333c3396efa430363924877905
anastasiia <evdokimova.arh@gmail.com>
2024-06-24 17:33:06 +0300

    [P-Р-54] (ADD) - procrm_custom_crm_lead: button "knowledge" was added to crm lead form

---
+++ procrm_custom_crm_lead/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom crm lead",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "author": "YevdokimovA",
+    "application": "True",
+    "depends": ["base", "web", "crm", "sale"],
+    "data": [
+        "views/crm_lead_view.xml",
+        "views/document_page_view.xml",
+    ],
+    "installable": True,
+}
---
+++ procrm_custom_crm_lead/models/__init__.py
@@ -0,0 +1,1 @@
+from . import crm_lead, document_page
